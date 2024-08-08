879e0b0d04c38a536a7a20d416086c6fbdd050f5
ZainabImadulla <zainab.imadulla@icloud.com>
2024-08-09 00:12:07 +0530

    add cors for localhost:3000

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+from starlette.middleware.cors import CORSMiddleware
 from app.model.model import predict_image
 
 
@@ -11,6 +11,17 @@ class TextIn(BaseModel):
 
 class PredictionOut(BaseModel):
     clothing: str
+
+origins = [
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins= origins,  # Replace with your React app's domain
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
